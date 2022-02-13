9087e5a6d1f9e0ec58015aa7ca310a41de88be18
Manuel <troyanlp@gmail.com>
2022-02-13 15:44:17 +0000

    Created main game loop

--- .ccls-cache/@home@runner@Board-Games-C/main.cpp
+++ .ccls-cache/@home@runner@Board-Games-C/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+enum GAME_OPTIONS{
+  CHESS = 1,
+  CHECKERS = 2,
+  PARCHIS = 3,
+  EXIT = 4
+};
+
+void PrintGameOptions(){
+  std::cout << "1. Chess" << std::endl;
+  std::cout << "2. Checkers" << std::endl;
+  std::cout << "3. Parchis" << std::endl;
+  std::cout << "4. Exit" << std::endl;
 }
+
+int main() {
+  unsigned int input;
+  std::cout << "Welcome to C++ Board Games!" << std::endl;
+
+  do{
+    std::cout << "Choose the game you want to play: " << std::endl;
+    PrintGameOptions();
+    std::cin >> input;
+
+  }while(input != GAME_OPTIONS::EXIT);
+  std::cout << "Bye!" << std::endl;
+}
+
